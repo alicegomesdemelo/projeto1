0211213f7b8e658a2c4e300189c0d4d4ce1d9a42
alicegomesdemelo <141650648+alicegomesdemelo@users.noreply.github.com>
2023-09-24 00:25:04 -0400

    Update musicas.js

--- musicas.js
+++ musicas.js
@@ -1,26 +1,26 @@
 export default [
   {
-    src: "./src/Dude - musica1.mp3",
+    src: "./musicas/Dude - musica1.mp3",
     name: "Thats what i like - Bruno Mars",
   },
   {
-    src: "./src/Dude - musica2.mp3",
+    src: "./musicas/Dude - musica2.mp3",
     name: "Style - Taylor Swift",
   },
    {
-    src: "./src/Dude - musica1.mp3",
+    src: "./musicas/Dude - musica1.mp3",
     name: "Thats what i like - Bruno Mars",
   },
   {
-    src: "./src/Dude - musica2.mp3",
+    src: "./musicas/Dude - musica2.mp3",
     name: "Style - Taylor Swift",
   },
     {
-    src: "./src/Dude - musica1.mp3",
+    src: "./musicas/Dude - musica1.mp3",
     name: "Thats what i like - Bruno Mars",
   },
   {
-    src: "./src/Dude - musica2.mp3",
+    src: "./musicas/Dude - musica2.mp3",
     name: "Style - Taylor Swift",
   },
 ];
