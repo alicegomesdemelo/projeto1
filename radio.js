e7366605b31cd22d72d04694670f5e9443dd5cdd
alicegomesdemelo <141650648+alicegomesdemelo@users.noreply.github.com>
2023-09-24 00:47:08 -0400

    Update radio.js

--- radio.js
+++ radio.js
@@ -25,25 +25,25 @@ const music_list = [
         img : 'images/stay.png',
         name : 'Stay',
         artist : 'The Kid LAROI, Justin Bieber',
-        music : 'music/stay.mp3'
+        music : 'musicas/musica1.mp3'
     },
     {
         img : 'images/fallingdown.jpg',
         name : 'Falling Down',
         artist : 'Wid Cards',
-        music : 'music/fallingdown.mp3'
+        music : 'musicas/musica2.mp3'
     },
     {
         img : 'images/faded.png',
         name : 'Faded',
         artist : 'Alan Walker',
-        music : 'music/Faded.mp3'
+        music : 'musicas/musica1.mp3'
     },
     {
         img : 'images/ratherbe.jpg',
         name : 'Rather Be',
         artist : 'Clean Bandit',
-        music : 'music/Rather Be.mp3'
+        music : 'musicas/musica2.mp3'
     }
 ];
 
