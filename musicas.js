f6c5ae19b308ce123c729baeaad5e252624cdd48
alicegomesdemelo <141650648+alicegomesdemelo@users.noreply.github.com>
2023-09-23 23:23:52 -0400

    Create musicas.js

---
+++ musicas.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    src: "./src/Thats what i like - musica1.mp3",
+    name: "Thats what i like - Bruno Mars",
+  },
+  {
+    src: "./src/Style - musica2.mp3",
+    name: "Style - Taylor Swift",
+  },
+   {
+    src: "./src/Thats what i like - musica1.mp3",
+    name: "Thats what i like - Bruno Mars",
+  },
+  {
+    src: "./src/Style - musica2.mp3",
+    name: "Style - Taylor Swift",
+  },
+    {
+    src: "./src/Thats what i like - musica1.mp3",
+    name: "Thats what i like - Bruno Mars",
+  },
+  {
+    src: "./src/Style - musica2.mp3",
+    name: "Style - Taylor Swift",
+  },
+];
